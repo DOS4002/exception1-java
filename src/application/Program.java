cbda0f7b8974e39bc9bec826026c59ef56732429
DOS4002 <olegariosantos4002@gmail.com>
2025-07-01 15:08:21 -0300

    Very bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,12 @@
 package application;
 
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Contributor;
-import entities.LegalEntity;
-import entities.NaturalPerson;
+import model.entities.Reservation;
 
 public class Program {
 
@@ -17,57 +15,40 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		List<Contributor> listContributors = new ArrayList<>();
-
-		System.out.print("Enter the number of tax payers: ");
-		int n = sc.nextInt();
-		sc.nextLine();
-
-		System.out.println();
-		System.out.println("---------------------------------------------------------------------------------------");
-		for (int i = 0; i < n; i++) {
-			final String BOLD = "\033[1m";
-			final String RESET = "\033[0m";
-			System.out.println(BOLD + "Tax payer # " + (1 + i) + " data:" + RESET);
-			System.out.print("Individual or company (i/c)? ");
-			char response = sc.next().charAt(0);
-			sc.nextLine();
-			while (response != 'i' && response != 'c') {
-				System.out.print(BOLD + "ERROR. TRY AGAIN (i/c): " + RESET);
-				response = sc.next().charAt(0);
-				sc.nextLine();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Room number: ");
+		int number = sc.nextInt();
+		System.out.println("Check-in date (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.println("Check-out date (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: Check-out date must be after check-in date");
+		} else {
+			Reservation reservation = new Reservation (number, checkIn, checkOut);
+			System.out.println();
+			System.out.println("Enter data to update the reservation: ");
+			System.out.println("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.println("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			Date now = new Date();
+			if(checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Error in reservation: Reservation dates for update must be future");
 			}
-			if (response == 'i') {
-				System.out.print("Name: ");
-				String name = sc.nextLine();
-				System.out.print("Anual income: ");
-				Double anualIncome = sc.nextDouble();
-				sc.nextLine();
-				System.out.print("Health expenditures: ");
-				Double healthExtenses = sc.nextDouble();
-				listContributors.add(new NaturalPerson(name, anualIncome, healthExtenses));
-			} else {
-				System.out.print("Name: ");
-				String name = sc.nextLine();
-				System.out.print("Anual income: ");
-				Double anualIncome = sc.nextDouble();
-				sc.nextLine();
-				System.out.print("Number of employees: ");
-				Integer quantityEmployee = sc.nextInt();
-				listContributors.add(new LegalEntity(name, anualIncome, quantityEmployee));
-
+			else if (!checkOut.after(checkIn)) {
+				System.out.println("Error in reservation: Check-out date must be after check-in date");
 			}
-			System.out.println("---------------------------------------------------------------------------------------");
+			else {
+				reservation.updatedDates(checkIn, checkOut);
+				System.out.println("Reservation: " + reservation);
+			}
+			
+			
 		}
-		System.out.println();
-		System.out.println("TAXES PAID:");
-		Double sum = 0.0; 
-		for(Contributor c : listContributors) {
-			System.out.println(c.getName() + ": $" + String.format("%.2f", c.taxCalculation()));
-			sum += c.taxCalculation();
-		}
-		System.out.println();
-		System.out.println("TOTAL TAXES: $" + sum);
-
+		sc.close();
 	}
 }
